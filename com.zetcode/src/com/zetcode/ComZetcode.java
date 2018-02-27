64267a162beeb9820a9a6dcd89201455abd86a43
Unknown <prze.stepniak@gmail.com>
2018-02-27 22:05:53 +0100

    Created file
    added source

---
+++ com.zetcode/src/com/zetcode/ComZetcode.java
@@ -0,0 +1,15 @@
+
+package com.zetcode;
+
+/**
+ *
+ * @author Przemek
+ */
+public class ComZetcode {
+
+    
+    public static void main(String[] args) {
+        new frame();
+    }
+    
+}
---
+++ com.zetcode/src/com/zetcode/frame.java
@@ -0,0 +1,15 @@
+package com.zetcode;
+
+import java.awt.HeadlessException;
+import javax.swing.JFrame;
+
+public class frame extends JFrame{
+
+    public frame() throws HeadlessException {
+        super();
+        setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE);
+        add( new Board());
+        setSize( 400, 400);
+        setVisible(true);
+    }
+}
